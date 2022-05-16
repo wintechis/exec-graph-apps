b598b60296a5d961fcca036180ed0f7f71c21232
albinlokaj <albin.lokaj@fau.de>
2022-05-16 10:28:56 +0200

    Test graph with two different layout types

--- apps/exec-graph-web/src/app/app.tsx
+++ apps/exec-graph-web/src/app/app.tsx
@@ -1,13 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import '@react-sigma/core/lib/react-sigma.min.css';
+import TestGraphIterative from './components/TestGraph';
+import { useState } from 'react';
 
 export function App() {
+  const [layoutIterative, changeLayout] = useState(false)
+
+  function ChangeLayout () {
+    changeLayout(!layoutIterative);
+  }
+  
   return (
-    <>
-      <NxWelcome title="exec-graph-web" />
-      <div />
-    </>
+    <div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{float: "left"}} onClick={ChangeLayout}>Change Layout</button>
+      
+      <div className="GraphContainer">
+        <TestGraphIterative layoutIterative={layoutIterative}/>
+      </div>
+    </div>
   );
 }
 
---
+++ apps/exec-graph-web/src/app/components/TestGraph.tsx
@@ -0,0 +1,82 @@
+import { useEffect } from 'react';
+
+import data from '../../../../../libs/graph/data-source/src/lib/sample01.graph.json';
+
+import Graph from 'graphology';
+import random from 'graphology-layout/random';
+
+import {
+  ControlsContainer,
+  FullScreenControl,
+  SearchControl,
+  SigmaContainer,
+  useLoadGraph,
+  useSigma,
+} from '@react-sigma/core';
+
+import { animateNodes } from 'sigma/utils/animate';
+
+import { useLayoutCircular } from '@react-sigma/layout-circular';
+
+import { useLayoutForceAtlas2 } from '@react-sigma/layout-forceatlas2';
+import { LayoutForceAtlas2Control } from '@react-sigma/layout-forceatlas2';
+
+const LoadGraph = () => {
+  const loadGraph = useLoadGraph();
+
+  useEffect(() => {
+    const graph = new Graph();
+    const dataToMap = JSON.parse(JSON.stringify(data));
+    graph.import(dataToMap);
+    random.assign(graph);
+    loadGraph(graph);
+  }, [loadGraph]);
+
+  return null;
+};
+
+const FormatGraphIter = () => {
+  const sigma = useSigma();
+const { positions, assign } = useLayoutForceAtlas2({ iterations: 100 });
+
+  useEffect(() => {
+    animateNodes(sigma.getGraph(), positions(), { duration: 1000 });
+  }, [positions, sigma]);
+
+  useEffect(() => {
+    assign();
+  }, [assign, sigma]);
+
+  return null;
+};
+
+const FormatGraph = () => {
+  const sigma = useSigma();
+const { positions, assign } = useLayoutCircular();
+
+  useEffect(() => {
+    animateNodes(sigma.getGraph(), positions(), { duration: 1000 });
+  }, [positions, sigma]);
+
+  useEffect(() => {
+    assign();
+  }, [assign, sigma]);
+
+  return null;
+};
+
+export const TestGraph = ({ layoutIterative, }: { layoutIterative: boolean;}) => {
+  return (
+    <SigmaContainer>
+      <LoadGraph />
+      {layoutIterative ? <FormatGraphIter  /> : <FormatGraph />}
+      <ControlsContainer position="bottom-right">
+        <FullScreenControl />
+        {layoutIterative ? <LayoutForceAtlas2Control /> : ''}
+        <SearchControl />
+      </ControlsContainer>
+    </SigmaContainer>
+  );
+};
+
+export default TestGraph;
