5437f7aaeeccb53f12d353538e7120810226b6cc
Albin Lokaj <albin.lokaj@fau.de>
2022-06-23 16:10:23 +0200

    EG 010 Several Layout changes like label hover and colors

--- libs/explorer/src/lib/graph-view/icons/icons.tsx
+++ libs/explorer/src/lib/graph-view/icons/icons.tsx
@@ -1,13 +1,46 @@
-import person from '../icons/person.svg';
+import person from './Person.svg';
 import college from '../icons/CollegeOrUniversity.svg';
 import organization from '../icons/Organization.svg';
 import city from '../icons/City.svg';
-import questionmark from '../icons/QuestionMark.svg'
+import questionmark from '../icons/QuestionMark.svg';
 
 export const icons = {
-    City: city,
-    CollegeOrUniversity: college,
-    Organization: organization,
-    Person: person,
-    QuestionMark: questionmark,
-  };
+  nodes: {
+    City: { image: city, color: 'blue' },
+    CollegeOrUniversity: { image: college, color: 'yellow' },
+    Organization: { image: organization, color: 'red' },
+    Person: { image: person, color: 'green' },
+    QuestionMark: { image: questionmark, color: 'grey' },
+  },
+  // edges: {
+  //   educationConnection: 'CollegeOrUniversity',
+  //   studiedAt: 'CollegeOrUniversity',
+  //   educatedAt: 'CollegeOrUniversity',
+  //   workConnection: 'Organization',
+  //   dealingWith: 'Organization',
+  //   hasDealing: 'Organization',
+  //   hasAudit: 'Organization',
+  //   audited: 'Organization',
+  //   currentCEO: 'Organization',
+  //   isCEO: 'Organization',
+  //   auditedBy: 'Organization',
+  //   isOnSupervisoryBoardOf: 'Organization',
+  //   isOnManagementBoardOf: 'Organization',
+  //   currentEmployee: 'Organization',
+  //   employedInPast: 'Organization',
+  //   currentlyWorksAt: 'Organization',
+  //   hasWorkConnection: 'Organization',
+  //   hasAlumni: 'Organization',
+  //   hasRole: 'Organization',
+  //   pastRolesAt: 'Organization',
+  //   currentManagementBoard: 'Organization',
+  //   currentSupervisoryBoard: 'Organization',
+  //   roleWith: 'Organization',
+  //   birthPlaceConnection: 'City',
+  //   birthPlaceOf: 'City',
+  //   birthPlace: 'City',
+  //   author: 'Person',
+  //   smarterThan: 'Person',
+  //   knows: 'Person',
+  // },
+};
