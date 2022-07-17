faca3840b56208199141257dde42be75d6b6f2bc
jsdev <juliusstoerrle@gmx.de>
2022-07-17 20:30:03 +0200

    Fix data-source-remote exports

--- libs/graph/data-source-remote/src/index.ts
+++ libs/graph/data-source-remote/src/index.ts
@@ -5,3 +5,5 @@ export * from './lib/http/fetch-http-client';
 export * from './lib/sparql/sparql-validator';
 export * from './lib/sparql/sparql-repository';
 export * from './lib/remote-data-source';
+export * from './lib/web-worker/remote-data-source-webworker';
+export * from './lib/web-worker/factory';
